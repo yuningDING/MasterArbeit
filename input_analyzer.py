563dc9a675fca96f7d31587b912602f799653621
Yuning Ding <ninading.china@outlook.com>
2019-04-27 11:51:55 +0200

    complete input length analysis with average length.

--- input_analyzer.py
+++ input_analyzer.py
@@ -1,13 +1,20 @@
-with open('resources/asap_prompt_1.txt', 'r', encoding='utf-8') as file:
-    max_length = 0
-    min_length = 1024
-    for line in file:
-        if line is '\n':
-            continue
-        if len(line) > max_length:
-            max_length = len(line)
-        if len(line) < min_length:
-            min_length = len(line)
-    print('Max sentence length in this prompt is '+str(max_length))
-    print('Min sentence length in this prompt is '+str(min_length))
-    file.close()
+for i in range(1, 11):
+    with open('resources/asap_prompt_'+str(i)+'.txt', 'r', encoding='utf-8') as file:
+        max_length = 0
+        sum_length = 0
+        count_line = 0
+        min_length = 1024
+        for line in file:
+            if len(line) > max_length:
+                max_length = len(line)
+                sum_length += len(line)
+                count_line += 1
+            if len(line) < min_length:
+                min_length = len(line)
+                sum_length += len(line)
+                count_line += 1
+        print('Min sentence length in this prompt' + str(i) + ' is ' + str(min_length))
+        print('Max sentence length in this prompt' + str(i) + ' is ' + str(max_length))
+        print('Average sentence length in prompt ' + str(i) + ' is ' + str(sum_length/count_line))
+        print()
+        file.close()
