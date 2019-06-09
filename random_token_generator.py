b0ce4ee1b2f205cd986586dd2da2be49d2bed80a
yuning ding <ninading.china@outlook.com>
2019-06-09 20:23:42 +0200

    formatting.

--- random_token_generator.py
+++ random_token_generator.py
@@ -110,4 +110,3 @@ if __name__ == "__main__":
                     file.write(str(index) + '\t' + str(i) + '\t' + '0\t0\t' + sentence + '\n')
                     index += 1
             file.close()
-   
